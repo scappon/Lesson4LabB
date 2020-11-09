5fb6dd0a7952daa476acefdec5da34266b55fc7e
Samuel Cappon <scappon@illinois.edu>
2020-11-08 23:48:12 -0600

    first commit

---
+++ Lesson4LabB/Lesson4LabB/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lesson4LabB
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public string myText { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void button_Clicked(object sender, EventArgs e)
+        {
+            myLabel.Text = myText;
+        }
+    }
+}
